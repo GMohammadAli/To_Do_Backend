6425bf65e7165db08672c3918553fea266e768e9
GMohammadAli <gulammohammadali@gmail.com>
2022-07-14 15:15:39 +0530

    Controllers Updated

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,14 +10,16 @@ class UsersController < ApplicationController
 
     # GET /users/{username}
     def show
-        render json: @user, status: :ok
+        token = jwt_encode(user_id: @user.id)
+        render json: {user: @user, token: token }, status: :ok
     end 
 
     # POST /users
     def create 
         @user = User.new(user_params)
         if @user.save
-            render json: @user , status: :created
+            token = jwt_encode(user_id: @user.id)
+            render json: {user: @user, token: token }, status: :created
         else
             render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
         end
@@ -33,7 +35,11 @@ class UsersController < ApplicationController
 
     # DELETE /users/{username}
     def destroy 
+        @tasks = Task.all
+        @tasks.where(user_id: @user.id).destroy_all
         @user.destroy
+
+        render  json: { message:"User Deleted" }
     end
 
     private
