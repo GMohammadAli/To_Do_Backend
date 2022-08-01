cdbb32349a980242a2930210abd3431070540392
GMohammadAli <gulammohammadali@gmail.com>
2022-08-01 20:16:43 +0530

    Changed db to mysql

--- db/schema.rb
+++ db/schema.rb
@@ -11,17 +11,17 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2022_07_11_124404) do
-  create_table "tasks", force: :cascade do |t|
+  create_table "tasks", charset: "utf8mb4", force: :cascade do |t|
     t.text "title"
     t.string "description"
     t.string "status"
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
-  create_table "users", force: :cascade do |t|
+  create_table "users", charset: "utf8mb4", force: :cascade do |t|
     t.string "name"
     t.string "username"
     t.string "email"
